89dd0a3bc5828148c1fa71d7e62b1ea062037d34
Jakub Serwatka <kubaserw@gmail.com>
2022-05-05 22:05:38 +0200

    chore: install styled-components

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,18 +1,26 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  siteMetadata: {
-    title: `Things I Should Do`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-mantine", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+    siteMetadata: {
+        title: `Things I Should Do`,
+        siteUrl: `https://www.yourdomain.tld`,
     },
-    __key: "images"
-  }]
+    plugins: [
+        "gatsby-plugin-mantine",
+        "gatsby-plugin-styled-components",
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "./src/images/",
+            },
+            __key: "images",
+        },
+    ],
 };
 
 export default config;
